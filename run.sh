49120cb0416e013c95323088cb658c0896738698
bonelli <fedechicco@gmail.com>
2014-10-12 16:47:40 +0200

    fixed error with debug variable

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ fi
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
-DEBUG_MODE=${DEBUG:"false"}
+DEBUG_MODE=${DEBUG:-false}
 echo "=> Debug mode is set to: ${DEBUG_MODE}"
 asadmin start-domain --debug=${DEBUG_MODE} -w
