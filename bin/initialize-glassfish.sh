2ad054920ece0e1cba48bc3891d8b193e9258d17
Koert Zeilstra <koert@zencode.nl>
2015-06-03 19:44:48 +0200

    fix bugs

--- bin/initialize-glassfish.sh
+++ bin/initialize-glassfish.sh
@@ -7,6 +7,8 @@ root_dir=${bin_dir}/..
 password="admin"
 export password
 
+domains_dir=/opt/glassfish4/glassfish/domains
+
 if [ ! "$(ls -A ${domains_dir})" ]; then
     echo "${domains_dir} is empty, create test domain"
     asadmin create-domain --nopassword domain1
